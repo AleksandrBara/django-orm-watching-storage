b3e7efdcb0059fbc165ae2ec4c2fe2bc881f1a2d
AleksandrBara <alexbar130379@gmail.com>
2022-09-07 17:07:16 +0300

    Исправил замечания в passcard_info_view.py.

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,30 +1,19 @@
-from datacenter.models import is_visit_long
-from datacenter.models import Passcard
-from datacenter.models import Visit
-from django.shortcuts import render
-from django.utils.timezone import localtime
-from datacenter.models import get_duration
-from datacenter.models import format_duration
-from django.shortcuts import get_list_or_404
-from django.shortcuts import get_object_or_404
+from datacenter.models import get_duration, format_duration, Passcard, Visit, is_visit_long
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
+    passcard = Passcard.objects.all()
     this_passcard_visits = []
-    onwer_name = get_object_or_404(Passcard, passcode=passcode)
-    one_person_all_visits = get_list_or_404(Visit, passcard=onwer_name)
-    for visit in one_person_all_visits:
-        entered_date = localtime(visit.entered_at).date()
-        entered_time = localtime(visit.entered_at).time()
-        entered_at = f'дата: {entered_date}   время: {entered_time}'
-        visit_duration = get_duration(visit)
-        visit_format_duration = format_duration(visit_duration)
-        visit_chek = is_visit_long(visit)
+    card_onwer_name = get_object_or_404(Passcard,
+                                   passcode=passcode)
+    passcard_visits = get_list_or_404(Visit,
+                                            passcard=card_onwer_name)
+    for visit in passcard_visits:
         visit_info = {
-            'entered_at': entered_at,
-            'duration': visit_format_duration,
-            'is_strange': visit_chek
+            'entered_at': visit.entered_at,
+            'duration': format_duration(get_duration(visit)),
+            'is_strange': is_visit_long(visit)
         }
         this_passcard_visits.append(visit_info)
     context = {
