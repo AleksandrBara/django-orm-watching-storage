36948e76559c106b78803d2edf627362922c8aaa
AleksandrBara <alexbar130379@gmail.com>
2022-09-07 17:07:42 +0300

    Исправил замечания в storage_information_view.py.

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,25 +1,18 @@
-from datacenter.models import Passcard
-from datacenter.models import Visit
 from django.shortcuts import render
-from django.utils.timezone import localtime
-from datacenter.models import get_duration
-from datacenter.models import format_duration
+from datacenter.models import get_duration, format_duration, Visit
 
 
 def storage_information_view(request):
-    not_closed_visits = []
-    not_end_visits = Visit.objects.filter(leaved_at=None)
-    for visit in not_end_visits:
-        duration = get_duration(visit)
-        formatted_duration = format_duration(duration)
-        moscow_entered_time = f'дата: {localtime(visit.entered_at).date()}   время: {localtime(visit.entered_at).time()} '
+    who_inside = []
+    unfinished_visits = Visit.objects.filter(leaved_at__isnull=True)
+    for visit in unfinished_visits:
         visit_info = {
-            'who_entered': '{}'.format(visit.passcard.owner_name),
-            'entered_at': '{}'.format(moscow_entered_time),
-            'duration': '{}'.format(formatted_duration),
+            'who_entered': visit.passcard.owner_name,
+            'entered_at': visit.entered_at,
+            'duration': format_duration(get_duration(visit)),
         }
-        not_closed_visits.append(visit_info)
+        who_inside.append(visit_info)
     context = {
-        'non_closed_visits': not_closed_visits,
+        'non_closed_visits': who_inside,
     }
     return render(request, 'storage_information.html', context)
