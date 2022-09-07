e3f742df93f819e45b027675fd185fa82580c57b
AleksandrBara <alexbar130379@gmail.com>
2022-09-07 17:17:31 +0300

    Внес изменения в passcard_info_view.py.

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,19 +1,32 @@
+from datacenter.models import is_visit_long
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.utils.timezone import localtime
+from datacenter.models import get_duration
+from datacenter.models import format_duration
 
 
 def passcard_info_view(request, passcode):
     passcard = Passcard.objects.all()[0]
-    # Программируем здесь
+    this_passcard_visits = []
+    onwer_name = Passcard.objects.filter(passcode=passcode)[0]
+    one_person_all_visits = Visit.objects.filter(passcard=onwer_name)
 
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+    for visit in one_person_all_visits:
+        entered_date = localtime(visit.entered_at).date()
+        entered_time = localtime(visit.entered_at).time()
+        entered_at = f'дата: {entered_date}   время: {entered_time}'
+        visit_duration = get_duration(visit)
+        visit_format_duration = format_duration(visit_duration)
+        visit_chek = is_visit_long(visit)
+        visit_info = {
+            'entered_at': entered_at,
+            'duration': visit_format_duration,
+            'is_strange': visit_chek
+        }
+
+        this_passcard_visits.append(visit_info)
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
