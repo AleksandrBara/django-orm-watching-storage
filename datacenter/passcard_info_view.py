b0a4438610b7e6484ab3f67098f96192265e6f64
AleksandrBara <103279129+AleksandrBara@users.noreply.github.com>
2022-09-12 15:54:40 +0300

    Изменил название переменной.

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -4,10 +4,10 @@ from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 def passcard_info_view(request, passcode):
     this_passcard_visits = []
-    owner_name = get_object_or_404(Passcard,
-                                   passcode=passcode)
+    passcard_holder = get_object_or_404(Passcard,
+                                        passcode=passcode)
     one_person_all_visits = get_list_or_404(Visit,
-                                            passcard=owner_name)
+                                            passcard=passcard_holder)
     for visit in one_person_all_visits:
         visit_info = {
             'entered_at': visit.entered_at,
@@ -16,7 +16,7 @@ def passcard_info_view(request, passcode):
         }
         this_passcard_visits.append(visit_info)
     context = {
-        'passcard': owner_name,
+        'passcard': passcard_holder,
         'this_passcard_visits': this_passcard_visits
     }
     return render(request, 'passcard_info.html', context)
